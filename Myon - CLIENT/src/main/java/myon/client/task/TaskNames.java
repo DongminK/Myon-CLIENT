cc130ed0f5b586088a8e621db1a94a73d18ef246
DongminK <kdm0228@gmail.com>
2014-07-01 23:06:19 +0900

    Login 화면 테스트

---
+++ Myon - CLIENT/src/main/java/myon/client/task/TaskNames.java
@@ -0,0 +1,8 @@
+package myon.client.task;
+
+public class TaskNames {
+
+	public static String SEND_MESSAGE = "SEND_MESSAGE";
+	public static String GET_USER = "GET_USER";
+	
+}
---
+++ Myon - CLIENT/src/main/java/myon/client/ui/UINames.java
@@ -0,0 +1,9 @@
+package myon.client.ui;
+
+public class UINames {
+
+	public static String CHAT = "Chat";
+	public static String LOGIN = "Login";
+	public static String REGIST = "Regist";
+	
+}
